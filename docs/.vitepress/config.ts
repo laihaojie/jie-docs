de1bab62ed74645ca5aa09ad41189d1cac22a309
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-24 16:55:11 +0800

    chore: update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,23 +4,37 @@ export default defineConfigWithTheme({
   title: '阿杰',
   themeConfig: {
     nav: [
-      { text: '工具软件', link: '/utils/' },
+      {
+        text: '工具软件',
+        link: '/utils/',
+      },
       { text: '框架文档', link: '/framework/' },
       { text: '插件扩展', link: '/extension/' },
-      { text: '开发环境', link: '/develop/' },
+      {
+        text: '开发环境',
+        items: [
+          {
+            items: [
+              { text: 'Git操作', link: '/develop/git/tag' },
+              { text: 'Item C', link: '/item-3' },
+            ],
+          },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' },
+        ],
+      },
       { text: '视频教程', link: '/video/' },
     ],
     socialLinks: [
       { icon: 'github', link: 'https://github.com/laihaojie' },
     ],
     sidebar: {
-      '/utils/': [
+      '/develop/': [
         {
-          text: '工具软件',
+          text: 'Git操作',
           collapsible: true,
           items: [
-            // { text: 'Index', link: '/utils/' },
-            { text: 'A', link: '/utils/a' },
+            { text: 'tag', link: '/develop/git/tag' },
             { text: 'B', link: '/utils/b' },
           ],
         },
