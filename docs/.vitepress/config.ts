531d81ca89b89a6b73ab9f46b53876b70ecd5946
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-08-06 17:14:46 +0800

    doc: update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitepress'
+import { getMds } from '../../scripts'
 
 export default defineConfig({
   title: '阿杰',
@@ -58,112 +59,65 @@ export default defineConfig({
         {
           text: 'VsCode',
           collapsible: true,
-          items: [
-            { text: '插件', link: '/src/utils/vscode/' },
-            { text: '配置文件', link: '/src/utils/vscode/config' },
-          ],
+          items: getMds('/utils/vscode/'),
         },
       ],
       '/src/engineering/': [
         {
           text: '前端工程化',
           collapsible: true,
-          items: [
-            { text: 'eslint', link: '/src/engineering/' },
-            { text: '代码提交检查', link: '/src/engineering/pre-commit' },
-            { text: '命令行交互', link: '/src/engineering/prompts' },
-            { text: 'node脚本', link: '/src/engineering/scripts' },
-            { text: '包管理器限制', link: '/src/engineering/package-manager' },
-          ],
+          items: getMds('/engineering/'),
         },
       ],
       '/src/framework/': [
         {
           text: 'Github Actions',
           collapsible: true,
-          items: [
-            { text: '部署web', link: '/src/framework/github-actions/' },
-            { text: '部署node服务', link: '/src/framework/github-actions/node' },
-          ],
+          items: getMds('/framework/github-actions/'),
         },
         {
           text: 'uniapp',
           collapsible: true,
-          items: [
-            { text: '升级依赖', link: '/src/framework/uniapp/' },
-          ],
+          items: getMds('/framework/uniapp/'),
         },
         {
           text: 'React-Native',
           collapsible: true,
-          items: [
-            { text: '添加', link: '/src/framework/react-native/' },
-            { text: '自定义package', link: '/src/framework/react-native/package' },
-            { text: 'icon图标', link: '/src/framework/react-native/icons' },
-            { text: 'webview', link: '/src/framework/react-native/webview' },
-            { text: '消息推送', link: '/src/framework/react-native/message-push' },
-          ],
+          items: getMds('/framework/react-native/'),
         },
         {
           text: 'Android',
           collapsible: true,
-          items: [
-            { text: '签名文件', link: '/src/framework/android/' },
-          ],
+          items: getMds('/framework/android/'),
         },
         {
           text: 'MongoDB',
           collapsible: true,
-          items: [
-            { text: '安装', link: '/src/framework/mongodb/' },
-            { text: '在nestjs中使用', link: '/src/framework/mongodb/nestjs' },
-            { text: '增删改查', link: '/src/framework/mongodb/crud' },
-          ],
+          items: getMds('/framework/mongodb/'),
         },
       ],
       '/src/develop/': [
         {
           text: 'Node 相关',
           collapsible: true,
-          items: [
-            { text: '安装', link: '/src/develop/node/' },
-            { text: '部署', link: '/src/develop/node/deploy' },
-            { text: '发布package', link: '/src/develop/node/publish' },
-            { text: 'nodemon', link: '/src/develop/node/nodemon' },
-          ],
+          items: getMds('/develop/node/'),
         },
         {
           text: 'Git 相关',
           collapsible: true,
-          items: [
-            { text: '常用命令', link: '/src/develop/git/' },
-            { text: '配置', link: '/src/develop/git/config' },
-            { text: 'tag', link: '/src/develop/git/tag' },
-          ],
+          items: getMds('/develop/git/'),
         },
         {
           text: '键盘侠',
           collapsible: true,
-          items: [
-            { text: 'vim', link: '/src/develop/vim/' },
-            { text: 'vscode', link: '/src/develop/vim/vscode' },
-            { text: '游乐场', link: '/src/develop/vim/playground' },
-          ],
+          items: getMds('/develop/vim/'),
         },
       ],
       '/src/notes/': [
         {
           text: '随手记',
           collapsible: true,
-          items: [
-            { text: '正则', link: '/src/notes/' },
-            { text: 'css', link: '/src/notes/css' },
-            { text: '各种命令', link: '/src/notes/command' },
-            { text: 'npmrc', link: '/src/notes/npmrc' },
-            { text: 'ssh-key', link: '/src/notes/sshkey' },
-            { text: '位运算', link: '/src/notes/bti' },
-            { text: 'markdown语法', link: '/src/notes/markdown' },
-          ],
+          items: getMds('/notes/'),
         },
       ],
     },
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable n/handle-callback-err */
 import path from 'path'
 import fs from 'fs-extra'
+import type { DefaultTheme } from 'vitepress'
 
-console.log(fs.readdirSync(path.resolve('./docs')))
-// fs.readdirSync(path.resolve('./docs'))
+const src_path = path.resolve(__dirname, '../docs/src')
+
+function readFiles(root_path) {
+  const dirs = fs.readdirSync(root_path)
+  for (const dir of dirs) {
+    const fullPath = path.join(root_path, dir)
+    // console.log(fullPath)
+    fs.stat(fullPath, (err, stats) => {
+      if (stats.isDirectory())
+        return readFiles(fullPath)
+      else
+        console.log(fullPath)
+    })
+  }
+}
+
+// readFiles(src_path)
+
+export function getMds(root_path): DefaultTheme.SidebarItem[] {
+  const dir_path = path.join(src_path, root_path)
+  const dirs = fs.readdirSync(dir_path)
+
+  return dirs.map((file) => {
+    const fullPath = path.join(dir_path, file)
+    const content = fs.readFileSync(fullPath, 'utf-8')
+    console.log(content.match(/^#\s+(.*)$/m))
+    const stats = fs.statSync(fullPath)
+    if (!stats.isDirectory()) {
+      return {
+        text: content.match(/^#\s+(.*)$/m)?.[1],
+        link: `/src${root_path}${file}`,
+      }
+    }
+    else {
+      return {}
+    }
+  }) as DefaultTheme.SidebarItem[]
+}
+console.log(getMds('/notes/'))
