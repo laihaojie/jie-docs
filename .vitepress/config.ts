b38df82f672d87aec6be309dea12bcb1f5c1058e
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-04-13 18:09:49 +0800

    chore: update config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -90,36 +90,36 @@ export default defineConfig({
     ],
     sidebar: {
       '/src/utils/': [
-        { text: 'VsCode', collapsible: true, items: getMds('/utils/vscode/') },
+        { text: 'VsCode', collapsed: false, items: getMds('/utils/vscode/') },
       ],
       '/src/engineering/': [
-        { text: '前端工程化', collapsible: true, items: getMds('/engineering/') },
+        { text: '前端工程化', collapsed: false, items: getMds('/engineering/') },
       ],
       '/src/framework/': [
-        { text: 'Github Actions', collapsible: true, items: getMds('/framework/github-actions/') },
-        { text: 'Uniapp', collapsible: true, items: getMds('/framework/uniapp/') },
-        { text: 'Flutter', collapsible: true, items: getMds('/framework/flutter/') },
-        { text: 'Electron', collapsible: true, items: getMds('/framework/electron/') },
-        { text: 'React-Native', collapsible: true, items: getMds('/framework/react-native/') },
-        { text: 'Android', collapsible: true, items: getMds('/framework/android/') },
-        { text: 'NestJs', collapsible: true, items: getMds('/framework/nestjs/') },
-        { text: 'MongoDB', collapsible: true, items: getMds('/framework/mongodb/') },
+        { text: 'Github Actions', collapsed: false, items: getMds('/framework/github-actions/') },
+        { text: 'Uniapp', collapsed: false, items: getMds('/framework/uniapp/') },
+        { text: 'Flutter', collapsed: false, items: getMds('/framework/flutter/') },
+        { text: 'Electron', collapsed: false, items: getMds('/framework/electron/') },
+        { text: 'React-Native', collapsed: false, items: getMds('/framework/react-native/') },
+        { text: 'Android', collapsed: false, items: getMds('/framework/android/') },
+        { text: 'NestJs', collapsed: false, items: getMds('/framework/nestjs/') },
+        { text: 'MongoDB', collapsed: false, items: getMds('/framework/mongodb/') },
       ],
       '/src/develop/': [
-        { text: 'Node 相关', collapsible: true, items: getMds('/develop/node/') },
-        { text: 'Docker', collapsible: true, items: getMds('/develop/docker/') },
-        { text: 'Git 相关', collapsible: true, items: getMds('/develop/git/') },
-        { text: '键盘侠', collapsible: true, items: getMds('/develop/vim/') },
-        { text: 'Centos', collapsible: true, items: getMds('/develop/centos/') },
-        { text: '其他', collapsible: true, items: getMds('/develop/other/') },
+        { text: 'Node 相关', collapsed: false, items: getMds('/develop/node/') },
+        { text: 'Docker', collapsed: false, items: getMds('/develop/docker/') },
+        { text: 'Git 相关', collapsed: false, items: getMds('/develop/git/') },
+        { text: '键盘侠', collapsed: false, items: getMds('/develop/vim/') },
+        { text: 'Centos', collapsed: false, items: getMds('/develop/centos/') },
+        { text: '其他', collapsed: false, items: getMds('/develop/other/') },
       ],
       '/src/notes/': [
-        { text: '随手记', collapsible: true, items: getMds('/notes/') },
+        { text: '随手记', collapsed: false, items: getMds('/notes/') },
       ],
       '/src/other/': [
         {
           text: '娜第',
-          collapsible: true,
+          collapsed: false,
           items: [
             {
               text: '娜第',
@@ -127,7 +127,7 @@ export default defineConfig({
             },
           ],
         },
-        { text: 'Python作业', collapsible: true, items: getMds('/other/python/') },
+        { text: 'Python作业', collapsed: false, items: getMds('/other/python/') },
       ],
     },
     algolia: {
