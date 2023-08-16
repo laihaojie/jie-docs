7eff59a0fa37eb94a77580a98ccff15d184a1416
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-08-16 16:07:29 +0800

    chore: update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -8,4 +8,11 @@ import './overrides.css'
 
 export default {
   ...Theme,
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  async enhanceApp({ app, router, siteData }) {
+    // 弹出输入框 账号密码
+    // window.prompt('请输入账号', 'admin')
+    // window.prompt('请输入密码', 'admin')
+    // await new Promise(resolve => setTimeout(resolve, 4000))
+  },
 }
