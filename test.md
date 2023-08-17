b0fdd3c7a9f8c7e9c76f3fa7b105fa8bce2c37c5
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-08-17 16:10:36 +0800

    chore: update

--- test.md
+++ test.md
@@ -5,3 +5,7 @@ const count = ref(1)
 </script>
 
 <div class="cursor-pointer select-none" @click="count++">{{count}}</div>
+
+# h1 {{count}}
+
+## h2 {{count}}
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { resolve } from 'node:path'
 import Unocss from 'unocss/vite'
 import type { UserConfig } from 'vite'
 
+import Inspect from 'vite-plugin-inspect'
+
 const vite: UserConfig = {
   plugins: [
     Unocss(),
+    Inspect(),
   ],
   resolve: {
     alias: {
