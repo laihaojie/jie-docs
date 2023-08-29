8d1d9caf2d0da64cb82352a1350e025fd736e72a
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-08-29 18:22:27 +0800

    chore: update

--- unocss.config.ts
+++ unocss.config.ts
@@ -10,10 +10,16 @@ export default defineConfig({
   presets: [
     presetWeb({ unit: 'px' }),
   ],
+  shortcuts: [
+    ['btn', 'px-4 py-1 rounded inline-block bg-primary text-[var(--vp-c-brand-darker)] cursor-pointer hover:bg-primary disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-primary'],
+    ['fc', 'flex items-center justify-center'],
+  ],
   theme: {
     colors: {
-      primary: '#ff0000',
-
+      primary: {
+        DEFAULT: 'var(--vp-c-brand-lighter)',
+      },
       success: '#00ff00',
     },
   },
