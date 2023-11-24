1737fd55c8b3dcbfae3ea44986ca4e9a76fce66a
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-11-24 18:57:23 +0800

    add info

--- .vitepress/layout/index.vue
+++ .vitepress/layout/index.vue
@@ -1,23 +1,33 @@
 <script setup lang="ts">
 import Theme from 'vitepress/theme'
+import { Toast, copyText } from 'root/utils'
 
 const info = {
   name: '阿杰',
   github: 'https://www.github.com/laihaojie',
-  email: ''
+  email: '17608421492@163.com',
+  wechat: 'fd_programmer',
 }
 
+async function copy(text) {
+  await copyText(text)
+  Toast('复制成功')
+}
 </script>
 
 <template>
   <Theme.Layout>
     <template #aside-outline-after>
-      <div class="p-10 fc mt-30 c-[var(--vp-c-brand)] fwbold flex flex-col">
+      <div class="p-10 fc mt-30 c-primary fwbold flex flex-col">
 
         <div class="text-20 mb-16">{{ info.name }}</div>
-        <div class="text-12"><a :href="info.github" target="_blank">GitHub: laihaojie</a> </div>
-        <div class="text-12">邮箱: 17608421492@163.com</div>
-        <div class="text-12">VX: l14367792992</div>
+        <div class="flex">
+          <a :href="info.github" target="_blank">
+            <div class="i-carbon:logo-github mx-4 cp" :title="info.github"></div>
+          </a>
+          <div class="i-carbon:email-new mx-4 cp" :title="info.email" @click="copy(info.email)"></div>
+          <div class="i-carbon:logo-wechat mx-4 cp" :title="info.wechat" @click="copy(info.wechat)"></div>
+        </div>
 
         <Eye class="mt-20"></Eye>
       </div>
---
+++ .vitepress/utils/index.ts
@@ -0,0 +1,31 @@
+export function Toast(msg, duration = 2000) {
+  const toastDOM = document.getElementById('custom_toast')
+  if (toastDOM)
+    document.body.removeChild(toastDOM)
+
+  const m = document.createElement('div')
+  m.id = 'custom_toast'
+  m.innerHTML = msg
+  m.style.cssText = 'color: rgb(255, 255, 255);background-color: rgba(0, 0, 0, 0.6);padding: 10px 15px;margin: 0 0 0 -60px;border-radius: 4px;position: fixed;    top: 50%;left: 50%;width: 130px;text-align: center;'
+  document.body.appendChild(m)
+  setTimeout(() => {
+    if (!document.body.contains(m)) return
+    const d = 0.5
+    m.style.opacity = '0'
+    setTimeout(() => {
+      document.body.removeChild(m)
+    }, d * 1000)
+  }, duration)
+}
+
+
+export function copyText(text: string) {
+  // return navigator.clipboard.writeText(text)
+
+  const input = document.createElement('input')
+  input.value = text
+  document.body.appendChild(input)
+  input.select()
+  document.execCommand('copy')
+  document.body.removeChild(input)
+}
