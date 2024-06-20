9b4ac6823dc0e5bfac2bffc623fb09f85e0ca250
阿杰 <53169820+laihaojie@users.noreply.github.com>
2024-06-20 20:07:52 +0800

    chore: update

--- .vitepress/components/exec.vue
+++ .vitepress/components/exec.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useEventListener } from '@vueuse/core'
+import { useEventListener, useLocalStorage } from '@vueuse/core'
 import { autoToast } from 'root/utils/autolog'
 import { type PropType, nextTick, ref, watch, computed } from 'vue'
 
@@ -18,8 +18,13 @@ const divInputDom = ref(null)
 const scrollDom = ref(null)
 const index = ref(0)
 const show = ref(false)
-const pwd = ref('')
-const pwdHtml = computed(() => `<span contenteditable="false" class="c-#12BC69 fw-bold">${pwd.value}&gt;&nbsp;</span>`)
+const shell = ref('')
+const cwd = useLocalStorage('cwd', '')
+const pwdHtml = computed(() => `<span contenteditable="false" class="${shell.value?'c-white':'c-#E5E510'} fw-bold"><span class="${shell.value?'c-white':'c-#12bc69'}">(${shell.value?'PowerShell':'bash'})</span> ${cwd.value}&gt;&nbsp;</span>`)
+
+watch(shell, ()=>{
+  setInputText()
+})
 
 function setInputText(c = '') {
   if (!c) index.value = 0
@@ -56,6 +61,17 @@ useEventListener(divInputDom, 'keydown', (e) => {
       setInputText(history.value[index.value]?.command)
     }
   }
+
+  // tab键
+  if (e.key === 'Tab') {
+    e.preventDefault()
+    const command = divInputDom.value.innerHTML.replace(pwdHtml.value, '')
+    const commands = props.commands.filter(i => i.startsWith(command))
+    if (commands.length) {
+      divInputDom.value.innerHTML = pwdHtml.value + commands[0]
+      focus()
+    }
+  }
 })
 
 useEventListener('keydown', (e) => {
@@ -73,13 +89,22 @@ watch(show, (v) => {
 })
 
 const isConnecting = ref(false)
-
 const baseApi = 'http://127.0.0.1:32677'
+function request(data) {
+  return fetch(baseApi, { method: 'POST', body: JSON.stringify({ ...data, shell: shell.value }) }).then(res => res.json())
+}
 
-fetch(baseApi, { method: 'POST', body: JSON.stringify({ cmd: 'pwd' }) }).then(res => res.json()).then((res) => {
+request({ cmd: 'pwd', cwd: cwd.value }).then(async (res) => {
   if (res.code === 1) {
     isConnecting.value = true
-    pwd.value = res.data.pwd
+    if (res.data.cwd)
+      cwd.value = res.data.cwd
+    else
+      await request({ cmd: 'pwd' }).then((r) => {
+        if (r.code === 1) {
+          cwd.value = r.data.cwd
+        }
+      })
   }
   else {
     isConnecting.value = false
@@ -100,23 +125,27 @@ function exec() {
     setInputText()
     return
   }
-  fetch(baseApi, {
-    method: 'POST',
-    body: JSON.stringify({ cmd: command }),
-  }).then(res => res.json()).then((r) => {
-    // eslint-disable-next-line no-console
-    import.meta.env.MODE === "development" && console.log(r)
-    history.value.push({
-      command,
-      data: `${divInputDom.value.innerHTML}\n${r.data.data}`
-    })
-    pwd.value = r.data.pwd
-    setInputText()
 
-    // 如果出现滚动条，滚动到底部
-    nextTick(() => {
-      scrollDom.value.scrollTop = scrollDom.value.scrollHeight
-    })
+  request({ cmd: command, cwd: cwd.value }).then((r) => {
+    if (r.code === 1) {
+      // eslint-disable-next-line no-console
+      import.meta.env.MODE === "development" && console.log(r)
+      history.value.push({
+        command,
+        data: `${divInputDom.value.innerHTML}\n${r.data.data}\n<br>`
+      })
+      if (r.data.cwd)
+        cwd.value = r.data.cwd
+      setInputText()
+
+      // 如果出现滚动条，滚动到底部
+      nextTick(() => {
+        scrollDom.value.scrollTop = scrollDom.value.scrollHeight
+      })
+    } else {
+      autoToast(r.data)
+    }
+
   }).catch((e) => {
     autoToast(e)
   })
@@ -127,8 +156,8 @@ function focus() {
   document.execCommand('selectAll', false, null)
   document.getSelection().collapseToEnd()
 }
-function handleFocus(e){
-  if(e.target !== divInputDom.value && !divInputDom.value.contains(e.target)){
+function handleFocus(e) {
+  if (e.target !== divInputDom.value && !divInputDom.value.contains(e.target)) {
     focus()
   }
 }
@@ -137,7 +166,12 @@ function handleFocus(e){
 <template>
   <div class="absolute right-10 bottom-10 w-580 h-500 bg-black rounded-10 p10">
     <div class="relative w-full h-full flex flex-col">
-      <div class="i-carbon-close-outline absolute right-0 top-0 cp" @click="close()"> </div>
+      <div class="absolute right-0 top-0 cp flex items-center gap-x-10">
+        <div title="Git Bash" @click="shell = ''">Bash</div>
+        <div title="PowerShell" @click="shell = 'powershell.exe'">PowerShell</div>
+        <div class="i-carbon-close-outline" @click="close()"> </div>
+      </div>
+
       <template v-if="show">
         <template v-if="isConnecting">
           <div class="flex flex-wrap gap-y-10 gap-x-20 mb-10">
